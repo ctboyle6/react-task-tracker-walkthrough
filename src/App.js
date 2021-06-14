7a84e58cde985aafbe6bfc5da36cda540943fc97
Chris Boyle <cthboyle6@gmail.com>
2021-06-14 16:30:40 -0400

    init Tasks component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+    const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'take out the trash'
+    },
+    {
+      id: 2,
+      text: 'wash the patio'
+    },
+    {
+      id: 3,
+      text: 'walk woody'
+    }
+  ])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
